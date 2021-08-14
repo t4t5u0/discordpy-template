1471e0c2276a9fc82e903f5ff595ad82a8d5ffff
t4t5u0 <ymmtryk0902@gmail.com>
2021-08-15 03:45:56 +0900

    Update: pre-commitの設定方法を書いた

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # discordpy-template
+
+
+
+
+開発する前にやるとよさそうなこと
+
+pre-commitの設定
+シークレットをコミットしようとしたら失敗するようにする
+```bash
+sudo apt install git-secrets
+git secrets --add '[a-Z0-9]{60}' 
+git secrets install
+```
+
+
+git管理下にあるファイルの更新を無視する
+```bash
+git update-index --assume-unchanged config/discord_secret.json
+```
--- src/main.py
+++ src/main.py
@@ -10,8 +10,8 @@ from lib.get_token import get_token
 def main():
     TOKEN = get_token()
     if not TOKEN:
-        print("TOKENが設定されていません。プログラムを終了します")
-        return 
+        print("TOKENが設定されていません。プログラムを終了します。")
+        return
     bot: Bot = Bot(command_prefix="!")
     # ここにコグを追加していく
     bot.load_extension("cog.on_ready")
@@ -20,6 +20,7 @@ def main():
         bot.run(TOKEN)
     except discord.errors.LoginFailure:
         print("Login failed")
+
 
 if __name__ == "__main__":
     main()
