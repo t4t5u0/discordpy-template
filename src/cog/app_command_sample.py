28a28a7372f4f5b281621618e42752c9950f5c21
t4t5u0 <ymmtryk0902@gmail.com>
2022-09-01 11:38:05 +0900

    Update: 返信を非公開にすべきでないコマンドが非公開になってたので

--- src/cog/app_command_sample.py
+++ src/cog/app_command_sample.py
@@ -12,7 +12,7 @@ class SlashCommandTestCog(commands.GroupCog):
     @app_commands.command(name="command-1")
     async def my_command(self, interaction: discord.Interaction) -> None:
         """/command-1"""
-        await interaction.response.send_message("Hello from command 1!", ephemeral=True)
+        await interaction.response.send_message("Hello from command 1!", ephemeral=False)
 
     @app_commands.command(name="command-2")
     # @app_commands.guilds(discord.Object(id=...), ...)
