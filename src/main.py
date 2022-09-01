e6fb91d8c918cba670c044229beeb53ea31c310c
t4t5u0 <ymmtryk0902@gmail.com>
2022-09-01 09:14:15 +0900

    Update: 正しくDockerで起動できるようにした

--- src/main.py
+++ src/main.py
@@ -1,11 +1,11 @@
-from typing import Optional
+from typing import Any, Optional
 
 import discord
-from async_timeout import Any
 from discord.ext.commands import Bot
-from discord.ext.commands.help import HelpCommand
+from discord import app_commands
 
-# from discord.ext.commands.bot import PrefixType, BotT
+# from discord.ext.commands.bot import BotT, PrefixType
+from discord.ext.commands.help import HelpCommand
 
 from help.help_command import JapaneseHelpCommand
 from libs.get_token import get_token
@@ -14,16 +14,20 @@ from libs.get_token import get_token
 class MyBot(Bot):
     def __init__(
         self,
-        command_prefix: Any, # acutally PrefixType[BotT]
+        command_prefix: Any,  # acctuary PrefixType[BotT],
         help_command: Optional[HelpCommand] = ...,
         **options: Any
     ) -> None:
-        super().__init__(command_prefix, help_command, **options)
+        super().__init__(
+            command_prefix=command_prefix, help_command=help_command, **options
+        )
 
     async def setup_hook(self) -> None:
         # ここにCogを追加していく
         await self.load_extension("cog.on_ready")
         await self.load_extension("cog.test_command")
+        await self.load_extension("cog.tasks")
+        # await self.load_extension("cog.app_command_sample")
 
 
 def main():
@@ -34,7 +38,7 @@ def main():
 
     intents = discord.Intents.default()
     intents.messages = True
-    # intents.message_content = True
+    intents.message_content = True
 
     # プレフィックスを変更
     prefix = "!"
